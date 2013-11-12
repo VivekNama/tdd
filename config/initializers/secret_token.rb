56061c26c362ea87b000a7ee763a7c6c846d1cb6
Vivek Nama <vivek.nama@gmail.com>
2013-11-11 16:57:05 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TddPractice::Application.config.secret_token = 'a73e6eb340553a2d73412e53150026c984b247326053c3ed88a9a96e7f0432c36a1b2c9807a6a331003ed29750d1dc2b3ae9025e7deae20ec8c851c08b754825'
